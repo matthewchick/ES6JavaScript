ee5912aade1a9d9ba84a722ed7196bc0a6dd0ae1
Matthew Chick <c.chick@unsw.edu.au>
2019-09-13 09:27:10 +1000

    using template string  and default parameter inside the function

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 // Use var to allow variable name to be global
 // Use let to allow variable name to be local
+/*
 if (true) {
     // var name ="Matthew";
     let name = 'Matthew';
+
 }
 alert(name);
+*/
+let fname = 'Matthew';
+let lname = "Chik";
+let age = prompt("Guess Matthew's age ..");
+
+// old way
+// let result = fname + ' '+lname+ ' is ' + age + ' years old';
+// alert(result);
+
+// using template string  - ES6
+let result = `${fname} ${lname} is ${age} years old`;
+alert(result);
+
+//use default parameter  - ES6
+function welcome(user="Jeremy", message= 'Hello') {
+    alert(`Hello ${user},${message}`);
+}
+welcome();
+welcome('Matthew', 'Good Afternoon');
