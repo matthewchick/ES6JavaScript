a04851169ae107d834d4cf64dc5e58ac19e64aef
Matthew Chick <c.chick@unsw.edu.au>
2019-09-13 09:43:52 +1000

    use arrow function

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Use var to allow variable name to be global
-// Use let to allow variable name to be local
+// 1.  Use let to allow variable name to be local - ES6
 /*
 if (true) {
     // var name ="Matthew";
@@ -8,21 +8,44 @@ if (true) {
 }
 alert(name);
 */
-let fname = 'Matthew';
-let lname = "Chik";
-let age = prompt("Guess Matthew's age ..");
+//let fname = 'Matthew';
+//let lname = "Chik";
+//let age = prompt("Guess Matthew's age ..");
 
-// old way
-// let result = fname + ' '+lname+ ' is ' + age + ' years old';
-// alert(result);
+//old way
+//let result = fname + ' '+lname+ ' is ' + age + ' years old';
+//alert(result);
 
-// using template string  - ES6
-let result = `${fname} ${lname} is ${age} years old`;
-alert(result);
+//2. using template string  - ES6
+//let result = `${fname} ${lname} is ${age} years old`;
+//alert(result);
 
-//use default parameter  - ES6
-function welcome(user="Jeremy", message= 'Hello') {
-    alert(`Hello ${user},${message}`);
-}
-welcome();
-welcome('Matthew', 'Good Afternoon');
+//3. use default parameter  - ES6
+//function welcome(user="Jeremy", message= 'Hello') {
+//    alert(`Hello ${user},${message}`);
+//}
+//welcome();
+//welcome('Matthew', 'Good Afternoon');
+
+//4. arrow function - ES6
+//function gretting(message) {
+//    return alert(`${message} everyone!`);
+//} 
+
+//let gretting = (message) => alert(`${message} everyone!`);
+//gretting('Good morning');
+
+let gretting = () => alert(`Hello everyone!`);
+gretting('Good morning');
+
+let createBlog = (title, body) => {
+    if (!title) {
+        throw new Error('A title is requires');
+    }
+    if (!body) {
+        throw new Error("Body can't be emply")
+    }
+    alert(`${title} - ${body}`);
+};
+
+createBlog('Blog title', 'Blog body');
