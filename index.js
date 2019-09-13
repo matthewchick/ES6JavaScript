af41c2d04731009b8522093432c61bd4660a2c77
Matthew Chick <c.chick@unsw.edu.au>
2019-09-13 11:32:01 +1000

    use ... spread and rest operator to combine two arrays or objects together

--- index.js
+++ index.js
@@ -150,4 +150,26 @@ var adventureClimbing = {
     //}
 };
 
-//8 Spread and rest operator
+//8 Spread and rest operator  ... combine two arrays or two objects 
+var mountains = ['Everest', 'Fish Tail', 'Annapurna'];
+var mountainsFromJapan = ['Fuji'];
+var allMountains = [...mountains, ...mountainsFromJapan];
+console.log(allMountains);
+
+var day = {
+    breakfast: 'toast with milk',
+    lunch: 'rice with chicken curry'
+}
+
+var night ={
+    dinner: 'noodle soup'
+}
+
+var picnic = {...day, ...night};
+console.log(picnic);
+
+//rest
+var rivers = ['Sunkoshi','Tamakoshi','Saptakoshi' ];
+var [first, ...rest] = rivers;
+console.log(first);
+console.log(rest);
